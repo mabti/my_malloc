8e3d062323617039ec1a27a5e1de321aaaa0bf5c
bastianm <bastian.mathison@outlook.com>
2023-10-19 19:17:01 +1100

    Generalised allocation functions

--- lib/os_mem.c
+++ lib/os_mem.c
@@ -6,14 +6,18 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
+#include <stddef.h>
 #include "global_mem.h"
 
-
+// stops clangd complaining
+#ifndef MEM_SIZE 
+#define MEM_SIZE 4098
+#endif
 
 struct malloc_mem_t g_malloc_mem = { 0 };
 
-void allocate_global_memory();
-void free_global_memory();
+static void *request_os_mem(size_t len);
+static void free_os_mem(void *ptr, size_t len);
 
 void init_my_malloc() {
 	if(g_malloc_mem.mem) // if already allocated
@@ -21,7 +25,7 @@ void init_my_malloc() {
 
 	// assign fields
 	g_malloc_mem.len = MEM_SIZE;
-	allocate_global_memory();
+	g_malloc_mem.mem = request_os_mem(MEM_SIZE);
 
 	// assign first block
 	g_malloc_mem.mem->len = MEM_SIZE;
@@ -34,7 +38,7 @@ void init_my_malloc() {
 }
 
 void quit_my_malloc() {
-	free_global_memory();
+	free_os_mem(g_malloc_mem.mem, g_malloc_mem.len);
 	g_malloc_mem.len = 0;
 	g_malloc_mem.mem = NULL;
 }
@@ -44,17 +48,17 @@ void quit_my_malloc() {
 
 #include <sys/mman.h>
 
-void allocate_global_memory() {
-	g_malloc_mem.mem = mmap(NULL,
-				MEM_SIZE,
-				PROT_READ | PROT_WRITE,
-				MAP_ANON | MAP_SHARED,
-				-1,
-				0);
+void *request_os_mem(size_t len) {
+	return mmap(NULL, 
+		len,
+		PROT_READ | PROT_WRITE, 
+		MAP_ANON | MAP_SHARED,
+		-1,
+		0);
 }
 
-void free_global_memory() {
-	munmap(g_malloc_mem.mem, g_malloc_mem.len);
+void free_os_mem(void *ptr, size_t len) {
+	munmap(ptr, len);
 }
 
 #endif
@@ -64,12 +68,20 @@ void free_global_memory() {
 
 #include <windows.h>
 
-void allocate_global_memory() {
-	g_malloc_mem.mem = HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, MEM_SIZE);
+// void allocate_global_memory() {
+// 	g_malloc_mem.mem = HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, MEM_SIZE);
+// }
+// 
+// void free_global_memory() {
+// 	HeapFree(GetProcessHeap(), 0, g_malloc_mem.mem);
+// }
+
+void *request_os_mem(size_t len) {
+	return HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, len);
 }
 
-void free_global_memory() {
-	HeapFree(GetProcessHeap(), 0, g_malloc_mem.mem);
+void free_os_mem(void *ptr, size_t _len) {
+	HeapFree(GetProcessHeap(), 0, ptr);
 }
 
 #endif
